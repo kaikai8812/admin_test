8d43d90ec300b62bbf8bdb0093bcf15805e9e67f
“kaikai8812” <sauna.kaikai@gmail.com>
2021-02-13 23:26:25 +0000

    devise導入完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+devise_for :masters, controllers: {
+  sessions:      'masters/sessions',
+  passwords:     'masters/passwords',
+  registrations: 'masters/registrations'
+}
+
+devise_for :users, controllers: {
+  sessions:      'users/sessions',
+  passwords:     'users/passwords',
+  registrations: 'users/registrations'
+}
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root to: 'homes#top'
 
 end
